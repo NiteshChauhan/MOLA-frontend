3bdb37138ec426c3301b0465286ee7eac751ea78
Niteshpal Singh <niteshsingh22002@gmail.com>
2026-01-05 15:08:53 +0530

    Resolve qutill issue

--- components/cms/RichTextEditor.js
+++ components/cms/RichTextEditor.js
@@ -1,35 +1,44 @@
+// components/cms/RichTextEditor.js
 import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
-import { modules, formats } from "../../utils/quillConfig";
+import "react-quill/dist/quill.snow.css";
 
 const ReactQuill = dynamic(() => import("react-quill"), {
   ssr: false,
-  loading: () => <div className="p-4 border">Loading editor...</div>,
 });
 
 export default function RichTextEditor({ value, onChange }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-    import("react-quill/dist/quill.snow.css");
-  }, []);
-
-  if (!mounted) {
-    return <div className="min-h-[300px] border bg-gray-50 rounded-lg" />;
-  }
+  if (typeof window === "undefined") return null;
 
   return (
-    <div className="bg-white border rounded-lg overflow-hidden">
-      <ReactQuill
-        theme="snow"
-        value={value}
-        onChange={onChange}
-        modules={modules}
-        formats={formats}
-        placeholder="Write your content here..."
-        className="min-h-[300px]"
-      />
-    </div>
+    <ReactQuill
+      theme="snow"
+      value={value}
+      onChange={onChange}
+      modules={{
+        toolbar: [
+          [{ header: [1, 2, 3, false] }],
+          ["bold", "italic", "underline", "strike"],
+          [{ list: "ordered" }, { list: "bullet" }],
+          ["blockquote", "code-block"],
+          [{ align: [] }],
+          ["link", "image"],
+          ["clean"],
+        ],
+      }}
+      formats={[
+        "header",
+        "bold",
+        "italic",
+        "underline",
+        "strike",
+        "list",
+        "bullet",
+        "blockquote",
+        "code-block",
+        "align",
+        "link",
+        "image",
+      ]}
+    />
   );
 }
